8a2a2694995925ad3b8e00082960b1db77f7c7a9
Volkov Danil <volkovdanil91@gmail.com>
2016-01-18 12:43:49 +0600

    Added loader for no composer projects

---
+++ loader.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once './src/SQLBuilder/BaseSQLBuilder.php';
+require_once './src/SQLBuilder/MySQLBuilder.php';
+require_once './src/SQLBuilder/MSSQLBuilder.php';
+require_once './src/SQLBuilder/BaseExpression.php';
