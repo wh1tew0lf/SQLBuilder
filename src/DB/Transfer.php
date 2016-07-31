1eb04120a88bc7dcff4166d2a10b6e755a686aaf
Volkov Danil <volkovdanil91@gmail.com>
2016-07-31 11:37:39 +0600

    SQL works too

--- src/DB/Transfer.php
+++ src/DB/Transfer.php
@@ -61,14 +61,15 @@ class Transfer {
     private function processFields($row, $columnsMap, $params) {
         $newRow = array();
         foreach($row as $name => &$field) {
-            if (isset($columnsMap[$name])) {
-                $newName = $columnsMap[$name];
-                if (isset($params['handler'])) {
-                    $type = isset($params['sourceColumns'][$name]['type']) ? $params['sourceColumns'][$name]['type'] : false;
-                    $field = call_user_func($params['handler'], $name, $type, $field);
-                }
-                $newRow[$newName] = $this->toDB->quote($field);
+            if ((false !== $columnsMap) && !isset($columnsMap[$name])) {
+                continue;
             }
+            $newName = ((false !== $columnsMap) && isset($columnsMap[$name])) ? $columnsMap[$name] : $name;
+            if (isset($params['handler'])) {
+                $type = isset($params['sourceColumns'][$name]['type']) ? $params['sourceColumns'][$name]['type'] : false;
+                $field = call_user_func($params['handler'], $name, $type, $field);
+            }
+            $newRow[$newName] = $this->toDB->quote($field);
         }
         return $newRow;
     }
@@ -106,7 +107,7 @@ class Transfer {
                     if (in_array($name, $params['fields'])) {
                         unset($columns[$name]);
                     }
-                } elseif ($params['fields'][$name] != $name) {
+                } elseif (isset($params['fields'][$name]) && ($params['fields'][$name] != $name)) {
                     $columns[$params['fields'][$name]] = $columns[$name];
                     unset($columns[$name]);
                 }
@@ -154,7 +155,7 @@ class Transfer {
      * Creates array of columns map, that consist of pairs oldFieldName => newFieldName
      * @param array $columns
      * @param array $params
-     * @return array
+     * @return array|false
      */
     public function createColumnsMap($columns, $params) {
         $columnsMap = array();
@@ -172,18 +173,21 @@ class Transfer {
             unset($params['types'][0]);
         }
 
+        if (!isset($fieldsRule) && !isset($typesRule)) {
+            return false;
+        }
         foreach ($columns as $name => $column) {
             if (isset($fieldsRule)) {
                 if ('whitelist' === $fieldsRule) {
                     if (isset($params['fields'][$name])) {
                         $columnsMap[$name] = $params['fields'][$name];
                     }
-                } elseif('blacklist' === $fieldsRule) {
+                } elseif ('blacklist' === $fieldsRule) {
                     if (!in_array($name, $params['fields'])) {
                         $columnsMap[$name] = $name;
                     }
                 } else {
-                    $columnsMap[$name] = $params['fields'][$name];
+                    $columnsMap[$name] = isset($params['fields'][$name]) ? $params['fields'][$name] : $name;
                 }
             }
 
@@ -199,7 +203,7 @@ class Transfer {
                     if ($found) {
                         $columnsMap[$name] = $name;
                     }
-                } elseif('blacklist' === $typesRule) {
+                } elseif ('blacklist' === $typesRule) {
                     $found = false;
                     foreach ($params['types'] as $oldType => $newType) {
                         if (strstr($column['type'], $oldType)) {
@@ -253,8 +257,11 @@ class Transfer {
 
             $select = isset($select) ? $select : $this->fromDB->getSQLBuilder()->from($fromTable);
 
-            $columns = $this->processColumns($this->fromDB->getColumns($fromTable), $params);
-            $columnsMap = $this->createColumnsMap($this->fromDB->getColumns($fromTable), $params);
+            $useType = !(is_array($tableParams) && isset($tableParams['sql']));
+
+            $sourceColumns = (is_array($tableParams) && isset($tableParams['sql'])) ? $params['columns'] : $this->fromDB->getColumns($fromTable);
+            $columns = $this->processColumns($sourceColumns, $params);
+            $columnsMap = $this->createColumnsMap($sourceColumns, $params);
 
             if ($this->toDB->isTableExists($toTable)) {
                 if (!$this->toDB->isTableEqual($toTable, $columns)) {
@@ -271,7 +278,7 @@ class Transfer {
                 $continue = false;
                 if ($rows = $this->fromDB->execute($select->limit($this->portion)->offset($offset)->getSQL())->fetchAll(BasePDO::FETCH_ASSOC)) {
                     foreach ($rows as $row) {
-                        $row = $this->processFields($row, $columnsMap, array_merge($params, array('sourceColumns' => $this->fromDB->getColumns($fromTable))));
+                        $row = $this->processFields($row, $columnsMap, array_merge($params, array('sourceColumns' => $useType ? $sourceColumns : false)));
                         $insertUpdate = $this->toDB->getSQLBuilder()->insertOnDuplicateUpdate($toTable, $row);
 
                         if (false === $this->toDB->execute($insertUpdate)) {
